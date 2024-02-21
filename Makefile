5dbbf027779edd13f36b73d19a6c0f011df5842d
mekb <mekbgithub@icloud.com>
2024-02-21 20:35:10 +1100

    :recycle: Rewrite argument handling and template

--- Makefile
+++ Makefile
@@ -1,41 +1,21 @@
 include config.mk
 
-TOP_BUILD_DIR := build
-BUILD_DIR := $(TOP_BUILD_DIR)
-BIN_BUILD_DIR = $(BUILD_DIR)/bin
-OBJ_BUILD_DIR = $(BUILD_DIR)/obj
-MAN_BUILD_DIR = $(BUILD_DIR)/man
+CC ?= cc
+
+ORIG_BUILD_DIR = build
+BUILD_DIR := $(ORIG_BUILD_DIR)
+
+BIN_DIR = $(BUILD_DIR)/bin
+OBJ_DIR = $(BUILD_DIR)/obj
+OBJ_BINARY_DIR = $(BUILD_DIR)/objbin
 
 SRC_DIR = src
-SRCS := $(sort $(shell find '$(SRC_DIR)' -name '*.c'))
+SRC_BINARY_DIR = binary
 
-MAN_DIR = man
-MAN_PAGES =
+CPPFLAGS += -DTARGET='"$(TARGET)"'
 
-ifeq ($(shell test -d '$(MAN_DIR)' && echo a),a)
-	MAN=1
-	MAN_PAGES := $(sort $(shell find '$(MAN_DIR)' -name '*.[[:digit:]].md'))
-	ifeq ($(MAN_PAGES),)
-		MAN=0
-	endif
-else
-	MAN=0
-endif
-
-ifeq ($(origin INSTALL_DIR),undefined)
-	ifeq ($(shell id -u),0)
-		INSTALL_DIR := '/usr/local'
-	else
-		INSTALL_DIR := '$(HOME)/.local'
-	endif
-endif
-
-ifeq ($(origin BIN_INSTALL_DIR),undefined)
-	BIN_INSTALL_DIR := '$(INSTALL_DIR)/bin'
-endif
-
-ifeq ($(origin MAN_INSTALL_DIR),undefined)
-	MAN_INSTALL_DIR := '$(INSTALL_DIR)/share/man'
+ifdef VERSION
+	CPPFLAGS += -DVERSION='"$(VERSION)"'
 endif
 
 ifeq ($(RELEASE),1)
@@ -51,64 +31,51 @@ else
 	CPPFLAGS += -DDEBUG
 endif
 
-CPPFLAGS += -DEXEC='"$(EXEC)"'
+SRC_FILES := $(wildcard $(SRC_DIR)/*.c) $(EXTRA_SRC_FILES)
+BINARY_FILES := $(wildcard $(SRC_BINARY_DIR)/*) $(EXTRA_BINARY_FILES)
+OBJ_FILES := $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRC_FILES))
+OBJ_BINARY_FILES := $(patsubst $(SRC_BINARY_DIR)/%, $(OBJ_BINARY_DIR)/%.o, $(BINARY_FILES))
 
-OBJS := $(SRCS:$(SRC_DIR)/%.c=$(OBJ_BUILD_DIR)/%.o)
-MAN_BUILT_PAGES := $(MAN_PAGES:$(MAN_DIR)/%.md=$(MAN_BUILD_DIR)/%)
+MAN_SRC_DIR=man
+MAN_OUT_DIR=$(ORIG_BUILD_DIR)/man
 
-all: build
+MAN_SRC_FILES :=
+MAN_OUT_FILES :=
 
-build: buildtext $(BIN_BUILD_DIR)/$(EXEC)
+ifeq ($(shell test -d '$(MAN_SRC_DIR)' && echo 1),1)
+	MAN=1
+	MAN_SRC_FILES := $(wildcard $(MAN_SRC_DIR)/*.[0-9].md)
+	MAN_OUT_FILES := $(patsubst $(MAN_SRC_DIR)/%.md, $(MAN_OUT_DIR)/%, $(MAN_SRC_FILES))
+else
+	MAN=0
+endif
 
-$(BIN_BUILD_DIR)/$(EXEC): $(OBJS)
-	@printf "\e[93m==> \e[0;1mLinking executable %s…\e[0m\n" '$(EXEC)'
-	@mkdir -p '$(dir $@)'
-	@$(CC) $(LDFLAGS) $^ $(LDLIBS) -o '$@'
-	@if [ "$(RELEASE)" = "1" ]; then printf "\e[95m==> \e[0;1mStripping executable %s…\e[0m\n" '$(EXEC)'; strip --strip-unneeded $(BIN_BUILD_DIR)/$(EXEC); fi
-	@printf "\e[1;91m> \e[0;1mBuilt executable to %s…\e[0m\n" '$(BIN_BUILD_DIR)/$(EXEC)'
+all: $(BIN_DIR)/$(TARGET)
 
-$(OBJ_BUILD_DIR)/%.o: $(SRC_DIR)/%.c
-	@printf "\e[92m==> \e[0;1mCompiling %s…\e[0m\n" '$<'
-	@mkdir -p '$(dir $@)'
-	@$(CC) $(CPPFLAGS) $(CFLAGS) -c '$<' -o '$@'
+$(BIN_DIR)/$(TARGET): $(OBJ_BINARY_FILES) $(OBJ_FILES) | $(BIN_DIR)
+	$(CC) $(LDFLAGS) $(LDLIBS) $^ -o $@
 
-buildtext:
-	@printf "\e[1;91m> \e[0;1mBuilding %s…\e[0m\n" '$(EXEC)'
+$(OBJ_BINARY_DIR)/%.o: $(SRC_BINARY_DIR)/% | $(OBJ_BINARY_DIR)
+	xxd -i $< | $(CC) $(CFLAGS) $(CPPFLAGS) -x c -c - -o $@
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJ_DIR)
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
 
-manbuildtext:
-	@if [ "$(MAN)" != "1" ]; then printf "\e[1;93m> There are no man pages\e[0m\n"; exit 1; fi
-	@printf "\e[1;95m> \e[0;1mBuilding man pages…\e[0m\n"
-
-run: build
-	@printf "\e[1;94m> \e[0;1mRunning %s…\e[0m\n" '$(EXEC)'
-	@$(BIN_BUILD_DIR)/$(EXEC) $(RUNARGS)
-
-man: manbuildtext $(MAN_BUILT_PAGES)
-
-$(MAN_BUILD_DIR)/%: $(MAN_DIR)/%.md
-	@printf "\e[96m==> \e[0;1mCompiling man page %s…\e[0m\n" '$<'
-	@mkdir -p '$(dir $@)'
-	@pandoc -s -f markdown -t man - -o '$@' < '$<'
+$(BIN_DIR):
+	mkdir -p -- $(BIN_DIR)
+$(OBJ_DIR):
+	mkdir -p -- $(OBJ_DIR)
+$(OBJ_BINARY_DIR):
+	mkdir -p -- $(OBJ_BINARY_DIR)
+$(MAN_OUT_DIR):
+	mkdir -p -- $(MAN_OUT_DIR)
 
 clean:
-	@printf "\e[1;96m> \e[0;1mCleaning…\e[0m\n"
-	@rm -rf '$(TOP_BUILD_DIR)'
+	rm -f -- $(BIN_DIR)/$(TARGET) $(OBJ_FILES) $(MAN_OUT_FILES) $(OBJ_BINARY_FILES) || true
+	rmdir -- $(OBJ_BINARY_DIR) $(BIN_DIR) $(OBJ_DIR) $(BUILD_DIR) $(ORIG_BUILD_DIR) $(MAN_OUT_DIR) || true
 
-ifeq ($(MAN),1)
-install: build man
-else
-install: build
-endif
-	@if [ "$(RELEASE)" != "1" ]; then printf "\e[1;93m> Installing requires you to be in release mode!\e[0m\n"; exit 1; fi
-	@printf "\e[93m==> \e[0;1mInstalling %s to %s…\e[0m\n" '$(EXEC)' '$(BIN_INSTALL_DIR)'
-	@install -Dpm755 -- '$(BIN_BUILD_DIR)/$(EXEC)' '$(BIN_INSTALL_DIR)/$(EXEC)'
-	@if [ "$(MAN)" == "1" ]; then printf "\e[93m==> \e[0;1mInstalling man pages to %s…\e[0m\n" '$(MAN_INSTALL_DIR)'; fi
-	$(foreach page,$(MAN_BUILT_PAGES),@install -Dpm644 -- '$(page)' '$(subst $(MAN_BUILD_DIR),$(MAN_INSTALL_DIR),$(page))')
+man: $(MAN_OUT_FILES)
 
-uninstall:
-	@if [ "$(RELEASE)" != "1" ]; then printf "\e[1;93m> Uninstalling requires you to be in release mode!\e[0m\n"; exit 1; fi
-	@printf "\e[1;93m> \e[0;1mUninstalling %s…\e[0m\n" '$(EXEC)'
-	@printf "\e[93m==> \e[0;1mUninstalling %s from %s…\e[0m\n" '$(EXEC)' '$(BIN_INSTALL_DIR)'
-	@rm -f -- '$(BIN_INSTALL_DIR)/$(EXEC)'
-	@if [ "$(MAN)" == "1" ]; then printf "\e[93m==> \e[0;1mUninstalling man pages from %s…\e[0m\n" '$(MAN_INSTALL_DIR)'; fi
-	$(foreach page,$(MAN_BUILT_PAGES),@rm -f -- '$(subst $(MAN_BUILD_DIR),$(MAN_INSTALL_DIR),$(page))')
+$(MAN_OUT_DIR)/%: $(MAN_SRC_DIR)/%.md | $(MAN_OUT_DIR)
+	sed 's/INSERT_VERSION_HERE/$(VERSION)/g' < '$<' | pandoc -s -f markdown -t man - -o '$@'
+
+.PHONY: all clean man
--- src/main.c
+++ src/main.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <sys/statvfs.h>
 #include <stdint.h>
 #include <mntent.h>
-#include <errno.h>
+#include <getopt.h>
+#include <err.h>
 
 #define COLOR "\x1b[38;5;14m"
 #define RESET "\x1b[0m"
 #define BYTES_PADDING 100
 #define BYTES_FORMAT "%.2f"
 #define PERCENTAGE_FORMAT "%.2f"
+
+static struct option options_getopt[] = {
+        {"help",     no_argument, 0, 'h'},
+        {"version",  no_argument, 0, 'V'},
+        {"colour",   no_argument, 0, 'c'},
+        {"color",    no_argument, 0, 'c'},
+        {"json",     no_argument, 0, 'j'},
+        {"psuedofs", no_argument, 0, 'p'},
+        {"quiet",    no_argument, 0, 'q'},
+        {0,          0,           0, 0  }
+};
 
 char *display_bytes(unsigned long bytes) {
 	if (bytes == 0) return "0";
@@ -25,21 +36,21 @@ char *display_bytes(unsigned long bytes) {
 	}
 
 	double bytes_dec = 0;
-	if (suffixI > 0) bytes_dec = (double)(bytes % BYTES_PADDING) / BYTES_PADDING; // the extra decimal digits
-	bytes /= BYTES_PADDING; // bytes is actual byte count now
+	if (suffixI > 0) bytes_dec = (double) (bytes % BYTES_PADDING) / BYTES_PADDING; // the extra decimal digits
+	bytes /= BYTES_PADDING;                                                        // bytes is actual byte count now
 	char *temp_dec;
 	char *temp_zero_dec;
 	if (suffixI > 0) {
 		temp_dec = malloc(8);
 		sprintf(temp_dec, BYTES_FORMAT, bytes_dec); // use temp string so we can remove the 0 before the decimal point, 6.5G would look like 60.5G otherwise
-		temp_zero_dec = strchr(temp_dec, '.'); // find dot in the string
+		temp_zero_dec = strchr(temp_dec, '.');      // find dot in the string
 	}
 
 	char *str = malloc(24); // final string we return
 
 	const char *suffixes = "KMGTPEZY";
 	if (suffixI > 0) {
-		sprintf(str, "%li%s%c", bytes, temp_zero_dec, suffixes[suffixI-1]);
+		sprintf(str, "%li%s%c", bytes, temp_zero_dec, suffixes[suffixI - 1]);
 	} else {
 		sprintf(str, "%li", bytes); // don't put the temp string if there's no dot
 	}
@@ -48,18 +59,6 @@ char *display_bytes(unsigned long bytes) {
 		free(temp_dec);
 
 	return str;
-}
-
-void usage(char *argv0) {
-	fprintf(stderr, "\
-Usage: %s [-c] [-j] [-p] [-q] [filesystems...]\n"
-"	-c --color --colour : adds color to the output\n"
-"	-j --json : outputs in json\n"
-"	-p --psuedofs : outputs psuedo filesystems too\n"
-"	-q --quiet : only show mount and block usage on 1 line\n"
-"	filesystems can either be the mount directory (e.g /), or the disk file (e.g /dev/sda1)\n"
-"	omit filesystems to list all filesystems\n",
-	argv0);
 }
 
 void escape_string(char *string) {
@@ -72,92 +71,129 @@ void escape_string(char *string) {
 
 void print_usage(bool color, fsblkcnt_t total, fsblkcnt_t free, fsblkcnt_t avail, unsigned long block) {
 	fsblkcnt_t used = total - free;
-	double percent = (double)used / (double)total * 100.0;
+	double percent = (double) used / (double) total * 100.0;
 	char *s_used = display_bytes(used * block);
 	char *s_total = display_bytes(total * block);
 	char *s_free = display_bytes(free * block);
 	char *s_avail = display_bytes(avail * block);
 	if (color) {
-		printf("" RESET COLOR"%s"RESET" used ("COLOR PERCENTAGE_FORMAT"%%"RESET"), "COLOR"%s"RESET" total, "COLOR"%s"RESET" free, "COLOR"%s"RESET" available",
-			s_used, percent, s_total, s_free, s_avail);
+		printf("" RESET COLOR "%s" RESET " used (" COLOR PERCENTAGE_FORMAT "%%" RESET "), " COLOR "%s" RESET " total, " COLOR "%s" RESET " free, " COLOR "%s" RESET " available",
+		       s_used, percent, s_total, s_free, s_avail);
 	} else {
-		printf("%s used ("PERCENTAGE_FORMAT"%%), %s total, %s free, %s available",
-			s_used, percent, s_total, s_free, s_avail);
+		printf("%s used (" PERCENTAGE_FORMAT "%%), %s total, %s free, %s available",
+		       s_used, percent, s_total, s_free, s_avail);
 	}
 }
 
 int main(int argc, char *argv[]) {
-#define INVALID { usage(argv[0]); return 1; }
+	bool invalid = false;
+	int opt;
+
 	bool color_flag = 0;
 	bool json_flag = 0;
 	bool psuedofs_flag = 0;
 	bool quiet_flag = 0;
-	bool flag_done = 0;
-	char* fs[argc];
-	int fsc = 0;
 
-	for (int i = 1; i < argc; ++i) {
-		if (argv[i][0] == '-' && argv[i][1] != '\0' && !flag_done) {
-			if (argv[i][1] == '-' && argv[i][2] == '\0') flag_done = 1;
-			else {
-				if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--color") == 0 || strcmp(argv[i], "--colour") == 0) {
-					if (json_flag || color_flag) INVALID;
-					color_flag = 1;
-				} else
-				if (strcmp(argv[i], "-j") == 0 || strcmp(argv[i], "--json") == 0) {
-					if (json_flag || color_flag || quiet_flag) INVALID;
-					json_flag = 1;
-				} else
-				if (strcmp(argv[i], "-q") == 0 || strcmp(argv[i], "--quiet") == 0) {
-					if (json_flag || quiet_flag) INVALID;
-					quiet_flag = 1;
-				} else
-				if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--psuedofs") == 0) {
-					if (psuedofs_flag) INVALID;
-					psuedofs_flag = 1;
-				} else
-				INVALID;
-			}
-		} else {
-			fs[fsc++] = argv[i];
+	// argument handling
+	while ((opt = getopt_long(argc, argv, ":hVcjpq", options_getopt, NULL)) != -1) {
+		switch (opt) {
+			case 'h':
+				printf("Usage: %s [-c] [-j] [-p] [-q] [filesystems...]\n"
+				       "-h --help: Shows help text\n"
+				       "-V --version: Shows the version\n"
+				       "-c --color --colour: adds color to the output\n"
+				       "-j --json: outputs in json\n"
+				       "-p --psuedofs: outputs psuedo filesystems too\n"
+				       "-q --quiet: only show mount and block usage on 1 line\n"
+				       "filesystems can either be the mount directory (e.g /), or the disk file (e.g /dev/sda1)\n"
+				       "omit filesystems to list all filesystems\n",
+				       TARGET);
+				return 0;
+			case 'V':
+				printf("%s %s\n", TARGET, VERSION);
+				return 0;
+			default:
+				if (!invalid) {
+					switch (opt) {
+						case 'c':
+							if (json_flag || color_flag) invalid = true;
+							else
+								color_flag = 1;
+							break;
+						case 'j':
+							if (json_flag || color_flag || quiet_flag) invalid = true;
+							else
+								json_flag = 1;
+							break;
+						case 'q':
+							if (json_flag || quiet_flag) invalid = true;
+							else
+								quiet_flag = 1;
+							break;
+						case 'p':
+							if (psuedofs_flag) invalid = true;
+							else
+								psuedofs_flag = 1;
+							break;
+						default:
+							invalid = true;
+							break;
+					}
+				}
+				break;
 		}
 	}
 
-	if (json_flag) printf("[");
+	if (invalid || argc < optind)
+		errx(1, "Invalid usage, try --help");
 
 	FILE *f = setmntent("/proc/self/mounts", "r");
 	struct mntent *m;
 	bool first = false;
 
+	size_t fs_count = 0;
+	char *fs[argc - optind];
+
+	for (size_t i = 0, j = optind; j < argc; ++i, ++j, ++fs_count) {
+		fs[i] = argv[j];
+	}
+
+	if (json_flag) printf("[");
+
 	while ((m = getmntent(f))) {
 		if (!psuedofs_flag) {
 			if (m->mnt_fsname[0] != '/') continue;
-			if (m->mnt_dir[0]    != '/') continue;
+			if (m->mnt_dir[0] != '/') continue;
 		}
-		if (fsc > 0) {
+		if (fs_count) {
 			bool skip = true;
-			for (int i = 0; i < fsc; ++i) {
-				if (fs[i] == NULL) continue;
-				if (strcmp(m->mnt_fsname, fs[i]) != 0
-						&& strcmp(m->mnt_dir, fs[i]) != 0)
+			for (size_t i = 0; i < fs_count; ++i) {
+				if (!fs[i]) continue;
+				if (strcmp(m->mnt_fsname, fs[i]) != 0 && strcmp(m->mnt_dir, fs[i]) != 0)
 					continue;
-				fs[i] = NULL;
 				skip = false;
+				fs[i] = NULL;
 			}
 			if (skip) continue;
 		}
 		struct statvfs vfs;
-		if (statvfs(m->mnt_dir, &vfs) < 0) { fprintf(stderr, "statvfs: %s: %s\n", m->mnt_dir, strerror(errno)); } else if (vfs.f_blocks > 0) {
+		if (statvfs(m->mnt_dir, &vfs) < 0) {
+			err(1, "statvfs");
+		} else if (vfs.f_blocks > 0) {
 			if (json_flag) {
 				if (first) printf(",");
 				first = true;
 
 				printf("{\"mnt\":{");
 
-				printf("\"dir\":\""); escape_string(m->mnt_dir);
-				printf("\",\"fsname\":\""); escape_string(m->mnt_fsname);
-				printf("\",\"type\":\""); escape_string(m->mnt_type);
-				printf("\",\"opts\":\""); escape_string(m->mnt_opts);
+				printf("\"dir\":\"");
+				escape_string(m->mnt_dir);
+				printf("\",\"fsname\":\"");
+				escape_string(m->mnt_fsname);
+				printf("\",\"type\":\"");
+				escape_string(m->mnt_type);
+				printf("\",\"opts\":\"");
+				escape_string(m->mnt_opts);
 				printf("\",\"freq\":%i,", m->mnt_freq);
 				printf("\"passno\":%i", m->mnt_passno);
 
@@ -191,7 +227,7 @@ int main(int argc, char *argv[]) {
 			} else {
 				if (quiet_flag) {
 					if (color_flag) {
-						printf(RESET COLOR"%s"RESET" mounted at "COLOR"%s"RESET, m->mnt_fsname, m->mnt_dir);
+						printf(RESET COLOR "%s" RESET " mounted at " COLOR "%s" RESET, m->mnt_fsname, m->mnt_dir);
 					} else {
 						printf("%s mounted at %s", m->mnt_fsname, m->mnt_dir);
 					}
@@ -200,8 +236,8 @@ int main(int argc, char *argv[]) {
 					printf("\n");
 				} else {
 					if (color_flag) {
-						printf(RESET COLOR"%s"RESET" mounted at "COLOR"%s"RESET"\n", m->mnt_fsname, m->mnt_dir);
-						printf(RESET"type: "COLOR"%s"RESET", opts: "COLOR"%s"RESET"\n", m->mnt_type, m->mnt_opts);
+						printf(RESET COLOR "%s" RESET " mounted at " COLOR "%s" RESET "\n", m->mnt_fsname, m->mnt_dir);
+						printf(RESET "type: " COLOR "%s" RESET ", opts: " COLOR "%s" RESET "\n", m->mnt_type, m->mnt_opts);
 					} else {
 						printf("%s mounted at %s\n", m->mnt_fsname, m->mnt_dir);
 						printf("type: %s, opts: %s\n", m->mnt_type, m->mnt_opts);
@@ -226,14 +262,12 @@ int main(int argc, char *argv[]) {
 
 	if (json_flag) printf("]\n");
 
-	bool fail = false;
-	for (int i = 0; i < fsc; ++i) {
-		if (fs[i] != NULL) {
-			fprintf(stderr, "Filesystem %s not found\n", fs[i]);
-			fail = true;
-		}
+	int ret = 0;
+	for (size_t i = 0; i < fs_count; ++i) {
+		if (!fs[i]) continue;
+		fprintf(stderr, "Filesystem %s not found\n", fs[i]);
+		ret = 1;
 	}
-	if (fail) return 1;
 
-	return 0;
+	return ret;
 }
